dcc9b6bd6e68f2ad4419684504a6363a4f232eb7
bajzattimi <bajzat.timi9696@gmail.com>
2021-10-09 22:14:46 +0200

    Update emagyar_form.py

--- emagyar_form.py
+++ emagyar_form.py
@@ -1,6 +1,4 @@
 from bs4 import BeautifulSoup
-import lxml
-import requests
 
 def gen_sents(soup):
     lines = soup.find_all("line")
@@ -8,37 +6,37 @@ def gen_sents(soup):
         ref = line_tag.ref.string.strip()
         yield f'# ref: {ref}'
         for tok in line_tag.left_context.string.strip().split():
-            yield tok
+                yield tok
         for tok in line_tag.kwic.string.strip().split():
-            yield tok
+                yield tok
         for tok in line_tag.right_context.string.strip().split():
-            yield tok
+                yield tok
+        yield ''
 
-with open("akar_fni_384.xml", "r", encoding="utf8") as fh:
-    soup = BeautifulSoup(fh, "lxml")
+def main(inp_fn, out_fn):
+    with open(inp_fn, encoding='UTF-8') as inp_fh:
+        soup = BeautifulSoup(inp_fh, 'lxml')
 
-    corpus = soup.find("corpus")
-    if corpus is not None:
-        corpus_str = corpus.string.strip()
+        corpus = soup.find('corpus')
+        if corpus is not None:
+            corpus_str = corpus.string.strip()
 
-    hits = soup.find("hits")
-    if hits is not None:
-        hits_str = hits.string.strip()
+        hits = soup.find('hits')
+        if hits is not None:
+            hits_str = hits.string.strip()
 
-    query = soup.find("query")
-    if query is not None:
-        query_str = query.string.strip()
+        query = soup.find('query')
+        if query is not None:
+            query_str = query.string.strip()
 
-    print('form')
-    print(f'# corpus: {corpus_str}')
-    print(f'# hits: {hits_str}')
-    print(f'# query: {query_str}')
+    with open(out_fn, 'w', encoding='UTF-8') as out_fh:
+        print('form', file=out_fh)
+        print(f'# corpus: {corpus_str}', file=out_fh)
+        print(f'# hits: {hits_str}', file=out_fh)
+        print(f'# query: {query_str}', file=out_fh)
 
-    a_generator = gen_sents(soup)
-    for i in a_generator:
-        print(i)
+        for out_line in gen_sents(soup):
+            print(out_line, file=out_fh)
 
-
-
-
-
+if __name__ == '__main__':
+    main('akar_fni_384.xml', 'akar_fni_385.tsv')
