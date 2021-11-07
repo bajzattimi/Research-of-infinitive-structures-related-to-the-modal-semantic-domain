bda7ba56bee42198cf58e09f9af05b24598a7223
bajzattimi <bajzat.timi9696@gmail.com>
2021-11-07 18:49:48 +0100

    create

--- emagyarform_webcorpus.py
+++ emagyarform_webcorpus.py
@@ -1,21 +1,27 @@
 from bs4 import BeautifulSoup
 
-
 def gen_sents(soup):
+    subquerys = soup.find_all('subquery')
+    for subquery in subquerys:
+        if subquery is not None and subquery.string is not None:
+            subquerys_str = subquery.string.strip()
+            yield f'# subquery: {subquerys_str}'
     lines = soup.find_all("line")
     for line_tag in lines:
         if line_tag.ref is not None and line_tag.ref.string is not None:
             ref = line_tag.ref.string.strip()
             yield f'# ref: {ref}'
-        if line_tag.left_context is not None and line_tag.left_context.string is not None:
-            for tok in line_tag.left_context.string.strip().split():
+    for line_tag in lines:
+        if line_tag.left is not None and line_tag.left.string is not None:
+            for tok in line_tag.left.string.strip().split():
                 yield tok
         if line_tag.kwic is not None and line_tag.kwic.string is not None:
             for tok in line_tag.kwic.string.strip().split():
                 yield tok
-        if line_tag.right_context is not None and line_tag.right_context.string is not None:
-            for tok in line_tag.right_context.string.strip().split():
+        if line_tag.right is not None and line_tag.right.string is not None:
+            for tok in line_tag.right.string.strip().split():
                 yield tok
+
 
 
 def main(inp_fn, out_fn):
@@ -26,24 +32,20 @@ def main(inp_fn, out_fn):
         if corpus is not None:
             corpus_str = corpus.string.strip()
 
-        hits = soup.find('hits')
-        if hits is not None:
-            hits_str = hits.string.strip()
+        #subquerys = soup.find('subquery')#
+        #if subquerys is not None:#
+            #subquerys_str = hits.string.strip()#
 
-        query = soup.find('query')
-        if query is not None:
-            query_str = query.string.strip()
 
     with open(out_fn, 'w', encoding='UTF-8') as out_fh:
         print('form', file=out_fh)
         print(f'# corpus: {corpus_str}', file=out_fh)
-        print(f'# hits: {hits_str}', file=out_fh)
-        print(f'# query: {query_str}', file=out_fh)
+        #print(f'# hits: {hits_str}', file=out_fh)#
+        #print(f'# subquery: {subquerys}', file=out_fh)
 
         for out_line in gen_sents(soup):
             print(out_line, file=out_fh)
 
 
 if __name__ == '__main__':
-    main('akar_fni_384.xml', 'akar_fni_388.tsv')
-
+    main('akar_fni_500_webcorpus.xml', 'akar_fni_500_webcorpus.tsv')
