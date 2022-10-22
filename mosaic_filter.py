083100407f9b7cbebe49fe1232237caea7acef9b
Balázs Indig <dlazesz@users.noreply.github.com>
2022-10-23 00:01:45 +0200

    Fix mosaic lookup and add mosaic filter

--- mosaic_filter.py
+++ mosaic_filter.py
@@ -1,6 +1,8 @@
 import sys
+import gzip
 from pathlib import Path
-from itertools import tee, islice
+from collections import defaultdict
+from itertools import tee, islice, groupby
 from multiprocessing import Pool, cpu_count
 from argparse import ArgumentParser, ArgumentTypeError
 
@@ -62,35 +64,78 @@ def get_clauses(comment_lines, sent):
 
 def mosaic_to_tok(mosaic):
     ret = []
+    score = 0
     for word in mosaic:
         if word.startswith('lemma:'):
             ret.append({'lemma': word[6:]})
+            score += 2
         elif word.startswith('['):
             ret.append({'xpostag': word})
+            score += 1
         else:
             ret.append({'form': word})
-    return ret
+            score += 4
+    return ret, score
 
 
-def create_window(inp_fh, out_fh, mosaic):
-    mosaic_toks = mosaic_to_tok(mosaic.split())
-    mosaic_len = len(mosaic_toks)
+def determine_mosaic_length(mosaic):
+    with gzip.open(mosaic, 'rt', encoding='UTF-8') as mosaic_fh:
+        first_line = next(mosaic_fh)
+        first_line = first_line.strip().split()
+
+    return len(first_line) - 1  # Remove count!
+
+
+def create_window(inp_fh, out_fh, mosaic, threshold):
+    # 1. Determine the lehgth of all mosaic from the first one
+    mosaic_len = determine_mosaic_length(mosaic)
+    # 2. Cache all examples with matching length
+    example_clauses_with_matching_length = []
     for comment_lines, sent in parse_emtsv_format(inp_fh):
         clause_len = len(next((line for line in comment_lines if line.startswith(' clause: ')))[9:].split())
-        if clause_len != mosaic_len:  # TODO
+        if clause_len != mosaic_len:
             continue
+        example_clauses_with_matching_length.append((comment_lines, sent))
 
-        """
-        for mosaic_word, word in zip(mosaic_toks, sent):
-            if mosaic_word.items() > word.items():
+    with gzip.open(mosaic, 'rt', encoding='UTF-8') as mosaic_fh:
+        # 3. Group by freq and score group elements
+        for key, group_it in groupby(mosaic_fh, key=lambda x: x.strip().split()[0]):
+            if int(key) < threshold:
                 break
-        else:
-            print(' '.join(tok['form'] for tok in sent), file=out_fh)
-        """
-        if all(mosaic_word.items() <= word.items() for mosaic_word, word in zip(mosaic_toks, sent)):
-            print(' '.join(tok['form'] for tok in sent), file=out_fh)
-            # print(' '.join('#'.join((tok['form'], tok['lemma'], tok['xpostag'])) for tok in sent), file=out_fh)
+            mosaic_group = {}
+            mosaic_to_examples = defaultdict(set)
+            mos_group_freq_str = ''
+            for curr_mosaic in group_it:
+                mos_group_freq_ind = next(n for n, char_bigram in enumerate(ngram(curr_mosaic, 2), start=2)
+                                          if char_bigram[0].isnumeric() and char_bigram[1] == ' ')
+                mos_group_freq_str = curr_mosaic[:mos_group_freq_ind]
+                curr_mosaic = tuple(curr_mosaic.strip().split()[1:])
+                mosaic_toks, score = mosaic_to_tok(curr_mosaic)
+                mosaic_group[curr_mosaic] = score
+                # 4. For the matching clauses store the example clause
+                for comment_lines, sent in example_clauses_with_matching_length:
+                    if all(mosaic_word.items() <= word.items() for mosaic_word, word in zip(mosaic_toks, sent)):
+                        example_clause = next((line for line in comment_lines if line.startswith(' clause: ')))[9:]
+                        mosaic_to_examples[curr_mosaic].add(example_clause)
+            # 5. Group by example sets
+            examples_to_mosaic = defaultdict(set)
+            for mosaic_ngram, example_set in mosaic_to_examples.items():
+                examples_to_mosaic[frozenset(example_set)].add((mosaic_ngram, mosaic_group[mosaic_ngram]))
+            # 6. Get max score per example set and print mosaics with that score
+            for mosaic_set in examples_to_mosaic.values():
+                max_score = max(mos_score for _, mos_score in mosaic_set)
+                for mos, mos_score in sorted(mosaic_set, key=lambda x: (-x[1], x[0])):
+                    if mos_score == max_score:
+                        print(mos_group_freq_str, ' '.join(mos), sep='', file=out_fh)
+                    else:
+                        break  # Sorted by max score -> Reaching the first non-max scrore means no more max score
 # ####### BEGIN argparse helpers, needed to be moved into a common file ####### #
+
+
+def existing_file(string):
+    if not Path(string).is_file():
+        raise ArgumentTypeError(f'{string} is not an existing file!')
+    return string
 
 
 def existing_file_or_dir_path(string):
@@ -164,7 +209,8 @@ def parse_args():
     # nargs=? means one or zero values allowing -p without value -> returns const, if totally omitted -> returns default
     parser.add_argument('-p', '--parallel', type=int_greater_than_1, nargs='?', const=cpu_count(), default=1,
                         help='Process in parallel in N process', metavar='N')
-    parser.add_argument('-m', '--mosaic', type=str, metavar='MOSAIC NGRAM', required=True)
+    parser.add_argument('-m', '--mosaic', type=existing_file, metavar='MOSAIC NGRAM FILE', required=True)
+    parser.add_argument('-f', '--min-freq', dest='min_freq', type=int, default=1,  metavar='MOSAIC NGRAM FILE')
 
     args = parser.parse_args()
 
@@ -190,7 +236,8 @@ def main():
     args = parse_args()
     if args.parallel > 1 and args.output_path != '-':
         raise ValueError(f'Output must be STDOUT if processing parallel ({args.output_path}) !')
-    gen_inp_out_fn_pairs = gen_input_output_filename_pairs(args.input_path, args.output_path, (args.mosaic,))
+    gen_inp_out_fn_pairs = gen_input_output_filename_pairs(args.input_path, args.output_path,
+                                                           (args.mosaic, args.min_freq))
 
     if args.parallel > 1:
         with Pool(processes=args.parallel) as p:
