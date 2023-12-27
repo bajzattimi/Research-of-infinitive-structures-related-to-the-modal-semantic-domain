ac8e5e588b4b5d1eab47382b27be0717d0a4b82b
Balázs Indig <dlazesz@users.noreply.github.com>
2023-12-28 00:54:14 +0100

    Fix tmk_workflow.sh

--- tmk_workflow.sh
+++ tmk_workflow.sh
@@ -2,7 +2,7 @@
 
 ./venv/bin/python split_sketch_fields.py -i tmk_concordance.tsv -o tmk_concordance_emtsv.tsv 2> tmk_split_errors.log
 
-./venv/bin/python substitute_tags.py -f filter_params.yaml --lower-sent-start -i tmk_concordance_emtsv.tsv -o tmk_concordance_emtsv_subs.tsv
+./venv/bin/python substitute_tags.py -f filter_params.yaml --lower-sent-start --keep-duplicates -i tmk_concordance_emtsv.tsv -o tmk_concordance_emtsv_subs.tsv
 
 ./venv/bin/python punct_window.py -i tmk_concordance_emtsv_subs.tsv -o tmk_concordance_emtsv_clauses.tsv -f filter_params.yaml 2> punct_window.log
 grep "^# clause_SPL:" tmk_concordance_emtsv_clauses.tsv | sed 's/^# clause_SPL: //' > tmk_concordance_spl_clauses.tsv
@@ -11,10 +11,10 @@ grep "^# clause_SPL:" tmk_concordance_emtsv_clauses.tsv | sed 's/^# clause_SPL: 
 THIS_SCRIPT_DIR=$( dirname -- "$( readlink -f -- "$0" )" )
 for i in $(seq 9 -1 2); do
     echo "$i"
-    time (awk -v N="${i}" '{if (NF == $N) print $0}' tmk_concordance_spl_clauses.tsv | \
+    time (awk -v N="${i}" '{if (NF == N) print $0}' tmk_concordance_spl_clauses.tsv | \
           "${THIS_SCRIPT_DIR}/"mosaic.sh "${i}" | \
-          LC_ALL=C.UTF-8 sort --parallel=40 -S 40G -T /data/aramis/tmp | uniq -c | \
-          LC_ALL=C.UTF-8 sort -nr -S10G --parallel=5 -T /data/aramis/tmp | \
+          LC_ALL=C.UTF-8 sort --parallel=2 -S 1G -T /tmp | uniq -c | \
+          LC_ALL=C.UTF-8 sort -nr -S1G --parallel=2 -T /tmp | \
           pigz > "tmk_mosaic_${i}.gz")
 done
 
