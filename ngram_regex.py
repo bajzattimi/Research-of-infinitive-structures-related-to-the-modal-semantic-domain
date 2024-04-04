60ed2822600baf9c3caf420f05290107f328d111
bajzattimi <68318997+bajzattimi@users.noreply.github.com>
2024-04-04 20:46:12 +0200

    test2

--- ngram_regex.py
+++ ngram_regex.py
@@ -2,31 +2,36 @@ import re
 
 
 def main():
-    input_file_path = "test.txt"
+    input_file_path = "test_akar_5_webkorpusz.txt"
     output_file_path1 = "output.txt"
     output_file_path2 = "output2.txt"
 
     regex_patterns = [(r'\[\/Adj\]\[Nom\] \[\/N\]\[Acc\]', r'\[\/N\]\[Nom\]'),
-                      (r'(\[\/Adj\]\[Nom\] |\[\/Det\|Art\.Def\] )*\[\/N\]\[Nom]( egy | \[\/Adj\]\[Nom\] )*\[\/N\]\[Poss\]\[Acc\]', r'\[\/N\]\[Acc\]')]
+                      (r'(\[\/Adj\]\[Nom\] |\[\/Det\|Art\.Def\] )*\[\/N\]\[Nom] (egy |\[\/Adj\]\[Nom\] )*\[\/N\]\[Poss\]\[Acc\]', r'\[\/N\]\[Acc\]')]
     regex_patterns2 = [(r'(\[\/Adj\]\[Nom\] |\[\/Det\|Art\.Def\] )*\[\/N\]\[Poss\]\[Acc\]', r'\[\/N\]\[Acc\]')]
 
     try:
         with open(input_file_path, 'r', encoding='utf-8') as input_file, \
-             open(output_file_path1, 'r', encoding='utf-8') as output_file_1, \
-             open(output_file_path2, 'r', encoding='utf-8') as output_file_2:
+             open(output_file_path1, 'w+', encoding='utf-8') as output_file_1, \
+             open(output_file_path2, 'w+', encoding='utf-8') as output_file_2:
+
+            result_lines1 = []
+            result_lines2 = []
 
             for line in input_file:
+                modified_line = line
                 for pattern, replacement in regex_patterns:
-                    line = re.sub(pattern, replacement, line)
-                output_file_1.write(line)
-            for line in output_file_1:
-                for pattern, replacement in regex_patterns:
-                    line = re.sub(pattern, replacement, line)
-                output_file_1.write(line)
-            for line in output_file_1:
+                    modified_line = re.sub(pattern, replacement, modified_line)
+                result_lines1.append(modified_line)
+
+            for line in result_lines1:
+                modified_line = line
                 for pattern, replacement in regex_patterns2:
-                    line = re.sub(pattern, replacement, line)
-                output_file_2.write(line)
+                    modified_line = re.sub(pattern, replacement, modified_line)
+                result_lines2.append(modified_line)
+
+            output_file_1.writelines(result_lines1)
+            output_file_2.writelines(result_lines2)
 
         print("Code has completed the replacements")
 
@@ -36,4 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
