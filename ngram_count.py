346e315bf7f041c5c378c1ccafd2739071344347
bajzattimi <68318997+bajzattimi@users.noreply.github.com>
2024-04-07 00:36:00 +0200

    test_count_split_akar

---
+++ ngram_count.py
@@ -0,0 +1,20 @@
+def merge_duplicate_ngrams(input_file, output_file):
+    ngram_dict = {}
+
+    with open(input_file, 'r') as file:
+        for line in file:
+            frequency, ngram = line.strip().split('\t')
+            frequency = int(frequency)
+            if ngram in ngram_dict:
+                ngram_dict[ngram] += frequency
+            else:
+                ngram_dict[ngram] = frequency
+
+    sorted_ngrams = sorted(ngram_dict.items(), key=lambda x: x[1], reverse=True)
+
+    with open(output_file, 'w') as file:
+        for ngram, frequency in sorted_ngrams:
+            file.write(f"{frequency}\t{ngram}\n")
+
+
+merge_duplicate_ngrams('merged_output_akar_webkorpusz.txt', 'output_file.txt')
---
+++ split_ngrams_by_length.py
@@ -0,0 +1,18 @@
+def split_results_by_ngram_length(input_file):
+    ngram_files = {}
+
+    with open(input_file, 'r') as file:
+        for line in file:
+            frequency, ngram = line.strip().split('\t')
+            n = len(ngram.split())
+            if n not in ngram_files:
+                ngram_files[n] = []
+            ngram_files[n].append((frequency, ngram))
+
+    for n, ngrams in ngram_files.items():
+        output_file = f"{n}_gram_results.txt"
+        with open(output_file, 'w') as file:
+            for frequency, ngram in ngrams:
+                file.write(f"{frequency}\t{ngram}\n")
+
+split_results_by_ngram_length('output_file.txt')
